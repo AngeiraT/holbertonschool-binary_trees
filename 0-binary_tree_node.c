606d5ab3fe6b5042dcce3ed2f3473b77a734354d
AngeiraT <thaispur04@icloud.com>
2022-09-01 00:13:05 -0400

    added parent pointer

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *binary_tree_node - a function that creates a binary tree node
+ *@parent: pointer to the parent node of the node to create
+ *@value: value to put in the new node
+ * Return: new node
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newnode = NULL;
+    int n = 0;
+
+    /* allocate memory for newnode */
+    newnode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+    /* check if root empty */
+    if (n == -1)
+    {
+        return NULL;
+    }
+    /* Store value n in newnode */
+    newnode->parent = parent;
+    newnode->n = value;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return (newnode);
+
+}
