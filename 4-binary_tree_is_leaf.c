fff132cef5dde3ee002a55fe963e0537973672fa
AngeiraT <thaispur04@icloud.com>
2022-09-02 11:50:57 -0400

    betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,7 +4,8 @@
  * binary_tree_is_leaf - checks if a node is a leaf
  * The node which does not have any child node is called the leaf node
  * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise return true if neither left or right leaves exist
+ * Return: 1 if node is a leaf,
+ * otherwise return true if neither left or right leaves exist.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
