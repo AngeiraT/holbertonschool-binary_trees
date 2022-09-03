a7d5a1af6e4854a6ff289ba9ec541be539de88d3
AngeiraT <thaispur04@icloud.com>
2022-09-02 22:16:36 -0400

    betty style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,10 +6,13 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if(tree) /* if tree exist */
-    {
-       binary_tree_delete(tree->left); /* delete left node*/ 
-       binary_tree_delete(tree->right); /* delete right node*/
-    free(tree); /* free whole tree */ 
-    }
+	/* if tree exists */
+	if (tree)
+	{
+		/* delete left and right*/
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
+
